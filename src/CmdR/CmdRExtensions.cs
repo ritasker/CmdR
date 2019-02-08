9a2b53d048ffcfb5e1f0b12c51f10865f028103e
Richard Tasker <richard.tasker@jet2.com>
2019-02-08 16:55:49 +0000

    Register Validators And Handlers

--- src/CmdR/CmdRExtensions.cs
+++ src/CmdR/CmdRExtensions.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Reflection;
+using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,12 +10,34 @@ namespace CmdR
     {
         public static void AddCmdR(this IServiceCollection services)
         {
-            services.AddRouting();
-            
-            
-            
+            RegisterCommandValidators(services);
+            RegisterCommandHandlers(services);
         }
-        
+
+        private static void RegisterCommandHandlers(IServiceCollection services)
+        {
+            var handlers = Assembly.GetEntryAssembly().GetTypes().ToList()
+                .Where(typeof(CommandHandler<>).IsAssignableFrom)
+                .Where(t => !t.GetTypeInfo().IsAbstract);
+
+            foreach (var handler in handlers)
+            {
+                services.AddSingleton(typeof(CommandHandler<>), handler);
+            }
+        }
+
+        private static void RegisterCommandValidators(IServiceCollection services)
+        {
+            var validators = Assembly.GetEntryAssembly().GetTypes().ToList()
+                .Where(typeof(IValidator).IsAssignableFrom)
+                .Where(t => !t.GetTypeInfo().IsAbstract);
+
+            foreach (var validator in validators)
+            {
+                services.AddSingleton(typeof(IValidator), validator);
+            }
+        }
+
         public static void UseCmdR(this IApplicationBuilder app)
         {
             app.UseMiddleware<CmdRMiddleware>();
